1be31bb88b94ce5f80fc389caef1cbee7f96c9d0
Eric James <eric.james@yale.edu>
2019-06-17 16:36:28 -0400

    fix typo in iiif base path

--- config/initializers/riiif_initializer.rb
+++ config/initializers/riiif_initializer.rb
@@ -1,6 +1,6 @@
 #Riiif::Image.file_resolver.base_path = '/Users/erjhome/RubymineProjects/Amy_Natural_History/iiif-images'
 
-Riiif::Image.file_resolver.base_path = '/var/www/html/bertram-images'
+Riiif::Image.file_resolver.base_path = '/var/www/html/bartram-images'
 
 Riiif::ImagesController.class_eval do
        skip_before_action :authenticate_user!, only: [:show, :info]
