394735e41145b499cf73547b3a63d06d6888c473
Eric James <eric.james@yale.edu>
2019-08-28 11:00:15 -0400

    print controller route

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,7 @@ Rails.application.routes.draw do
 
   mount Riiif::Engine => '/image-service', as: 'riiif'
 
+  get 'print/scan/:scan' => 'print_scan#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
