dfb970b5a87208ebd8ae403daed784a1589b5a76
Eric James <eric.james@yale.edu>
2019-10-18 17:17:48 -0400

    add sort by relavance and id

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -100,7 +100,8 @@ class CatalogController < ApplicationController
     config.add_facet_field 'author_s', label: 'Notetaker (Objects)'
 
 
-    config.add_facet_field 'scan_author_s', label: 'Author'
+    config.add_facet_field 'scan_author_sm', label: 'Author (Objects)'
+    config.add_facet_field 'author_display', label: 'Author (Scans)'
     config.add_facet_field 'scan_part_of_s', label: 'Container'
     config.add_facet_field 'scan_location_s', label: 'Category'
 
@@ -211,10 +212,12 @@ class CatalogController < ApplicationController
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', label: 'relevance'
-    config.add_sort_field 'pub_date_sort desc, title_sort asc', label: 'year'
-    config.add_sort_field 'author_sort asc, title_sort asc', label: 'author'
-    config.add_sort_field 'title_sort asc, pub_date_sort desc', label: 'title'
+    #config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', label: 'relevance'
+    #config.add_sort_field 'pub_date_sort desc, title_sort asc', label: 'year'
+    #config.add_sort_field 'author_sort asc, title_sort asc', label: 'author'
+    #config.add_sort_field 'title_sort asc, pub_date_sort desc', label: 'title'
+    config.add_sort_field 'score desc', label: 'relevance'
+    config.add_sort_field 'id asc', label: 'id'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
